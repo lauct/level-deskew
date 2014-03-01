ca68a081adf5af737ec0b052e6edd40e6f40897d
Justin Liu <mangoliu@gmail.com>
2014-03-01 21:09:17 +0800

    support ext file name parameter

--- level+deskew.sh
+++ level+deskew.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
 ##################################################################
-# level+deskew.sh                                                   #
+# level+deskew.sh                                                #
 # Author: Lauct                                                  #
-# Purpose: Batch adjust color level for grayscale pics			 #
+# Purpose: Batch adjust color level for pics					 #
 # Date: 2014.02.16                                               #
 ##################################################################
 
 # for d in $DirName
 dir="_converted_"
-echo $dir
+
 mkdir -p $dir
 
-echo "\033[1;36;40m###################################\033[0m\n"
-echo "\033[1;31;40m Adjust contrast and deskewing files to $dir \033[0m\n" #tip something
-echo "\033[1;36;40m###################################\033[0m\n"
-
-for file in *.jpg # you can change file type here
-    do
-		convert -verbose $file -level 15% -quality 85% -alpha off +deskew $dir/$file.jpg
-    done
+if [[ -z $1 ]]; then
+	
+	echo "\033[1;36;40m請輸入欲處理的圖檔副檔名\033[0m\n";
+	exit 0;
+	
+	else 
+		echo "\033[1;36;40m處理過的圖檔將輸出至 $dir 目錄下\n"
+		echo "\033[1;36;40m###################################\033[0m\n";
+		echo "\033[1;31;40mAdjusting contrast and deskewing files to $dir \033[0m\n";
+		echo "\033[1;36;40m###################################\033[0m\n";
+		
+		for file in *.$1 # you can change file type here
+		    do
+				convert -verbose $file -level 15% -quality 85% -alpha off +deskew $dir/$file.jpg
+		done
+fi
+	
 exit 0
